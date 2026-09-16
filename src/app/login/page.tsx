dddfae665785adabc7fd7f282c3ea996b64f3682
qet-kultur-kompass <info@ralph-kuester.de>
2026-09-16 15:06:45 +0200

    Create page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/LoginForm";
+
+export const metadata = {
+  title: "Login – QET Kultur-Kompass",
+};
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
